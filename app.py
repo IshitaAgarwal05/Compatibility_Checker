43deca3a4ad104e3591d2337db1cf50ef7413d68
IshitaAgarwal05 <2005.agarwal.ishita@gmail.com>
2025-02-26 22:00:42 +0530

    initial update 5

--- app.py
+++ app.py
@@ -299,4 +299,5 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8501))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8501)))
+    index()
